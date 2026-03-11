64483c1e8f5f2913e988ebfa3c47b3c5d8c6d9f5
Binu0714 <binuj714@gmail.com>
2026-03-11 09:50:59 +0530

    implement model

--- Test-BackEnd/src/main/java/com/gms/testbackend/Model/Country.java
+++ Test-BackEnd/src/main/java/com/gms/testbackend/Model/Country.java
@@ -1,4 +1,12 @@
 package com.gms.testbackend.Model;
 
+import lombok.Data;
+
+@Data
 public class Country {
+    private String name;
+    private String capital;
+    private String region;
+    private long population;
+    private String flag;
 }
